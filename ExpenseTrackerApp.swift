810ace4781249fc9854d2b3a5662c1175e5fc454
Dylan-KA <111679436+Dylan-KA@users.noreply.github.com>
2024-10-08 16:49:12 +1100

    Initial commit

---
+++ ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Dylan Archer on 8/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
